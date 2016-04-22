ce6c1778d736265ea806aa17b4d3a26a6e0982ca
Ahmed Hawas <ahmed.hawas@thinkresearchgroup.com>
2016-04-21 22:57:51 -0400

    add react router and three simple routes

---
+++ src/components/resources.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export default () => {
+  return (
+    <div>
+      Secret Recipe
+      <ul>
+        <li>1 Cup Super</li>
+        <li>1 Cup Spice</li>
+        <li>1 Cup Everything nice</li>
+      </ul>
+    </div>
+  );
+};
